ff084dd32b4672dff54f7e6bdede1eb6b0a35d51
Rauljo <104280181+Rauljo@users.noreply.github.com>
2026-05-25 13:48:41 +0100

    Update message type assigner for new CSV files

--- Whatsapp_Code/message_type_assigner.py
+++ Whatsapp_Code/message_type_assigner.py
@@ -4,7 +4,7 @@ import random
 import pandas as pd
 
 # Read the CSVs and assign Custom or Basic randomly. Each CSV has two groups: CloseFriends=Yes or No. I want each group to be balanced.
-csvs = ["selin_contacts_clean_code.csv", "contacts_dani_code.csv", "friends_code.csv"]
+csvs = ["friends_code.csv", "friends1_code.csv", "friends2_code.csv"]
 
 for csv in csvs:
     df = pd.read_csv(csv)
